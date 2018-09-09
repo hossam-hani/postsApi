d8e0999740edf3415fd10cb501be014ad6b6ba0c
hossam hani makarm <hossamhani.t@gmail.com>
2018-09-10 00:30:05 +0200

    Create Login Function

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -36,6 +36,42 @@ class AuthController extends Controller
 
         return json_decode((string) $response->getBody(), true);
 
+    }
+
+
+    public function login(Request $request){
+
+        // validation 
+
+        //Check If user in DB
+        $user = User::where('email',$request->input('email'))->first();
+        if(!$user){
+           return response()->json(['status' => 'error', 'message' => 'User Not Found']); 
+        }
+
+        // check password
+        
+        if(!Hash::check($request->input('password'), $user->password)){
+            return response()->json(['stats' => 'error', 'message' => 'Password Incorrect']);
+        }
+
+
+
+        // Get The Token
+        $http = new Client;
+
+        $response = $http->post('http://posts.dev.hossam/oauth/token', [
+            'form_params' => [
+                'grant_type' => 'password',
+                'client_id' => '2',
+                'client_secret' => 'lU7g1t8TrtkiveTpLmpmAo4JIw1inVp3dvH7WvNY',
+                'username' => $request->input('email'),
+                'password' => $request->input('password'),
+                'scope' => '',
+            ],
+        ]);
+
+        return json_decode((string) $response->getBody(), true);
 
 
     }
