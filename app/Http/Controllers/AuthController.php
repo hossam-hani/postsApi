ee01a40372585ced5197b02df4f99fbbcf4ffed0
hossam hani makarm <hossamhani.t@gmail.com>
2018-09-10 00:11:49 +0200

    fix forget Import Hash Class

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\User;
 use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Hash;
+
 class AuthController extends Controller
 {
     public function register(Request $request){
